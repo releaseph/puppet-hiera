d37d714716fff2c16dcf9edbf61ef84468b0676f
Paolo Cadiz <pcadiz@cambridge.org>
2017-01-10 09:48:32 +0800

    added environment hierarchy

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,6 +4,10 @@ if $::ec2_tag_puppet_role {
 	$puppet_role = $::ec2_tag_puppet_role
 }
 
+if $::ec2_tag_puppet_env {
+	$puppet_env = $::ec2_tag_puppet_env
+}
+
 hiera_include('classes',[])
 node default {
   hiera_resources('resources',{})
