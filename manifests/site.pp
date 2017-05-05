5374eb0d01b966c8d0b2e509b76eac35173361c4
Paolo Cadiz <pcadiz@cambridge.org>
2017-05-05 10:52:02 +0800

    added variables for running_on

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,16 @@ if $::ec2_tag_puppet_env {
 	$puppet_env = $::ec2_tag_puppet_env
 }
 
+if $::is_virtual {
+  if $::ec2_instance_id {
+    $running_on = 'aws'
+  } else {
+    $running_on = $::virtual
+  }
+} else {
+  $running_on = 'bare-metal'
+}
+
 hiera_include('classes',[])
 node default {
   hiera_resources('resources',{})
