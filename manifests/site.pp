5b6e3960e9577a566c802fadec87c33d3eaced76
Paolo Cadiz <pcadiz@cambridge.org>
2017-05-09 10:11:46 +0800

    lint updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,11 @@
 include stdlib
 
 if $::ec2_tag_puppet_role {
-	$puppet_role = $::ec2_tag_puppet_role
+    $puppet_role = $::ec2_tag_puppet_role
 }
 
 if $::ec2_tag_puppet_env {
-	$puppet_env = $::ec2_tag_puppet_env
+    $puppet_env = $::ec2_tag_puppet_env
 }
 
 if $::is_virtual {
